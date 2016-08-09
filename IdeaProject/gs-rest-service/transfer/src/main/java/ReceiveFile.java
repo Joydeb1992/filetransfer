c6d57feafeb44d0efb55f67d351f7b9bd834defa
Joydeb1992 <joydeb.barman@redbus.com>
2016-08-09 11:06:45 +0530

    added

---
+++ IdeaProject/gs-rest-service/transfer/src/main/java/ReceiveFile.java
@@ -0,0 +1,56 @@
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+/**
+ * Created by joydebbarman on 5/8/16.
+ */
+
+
+public class ReceiveFile {
+
+    public final static int SOCKET_PORT = 13267;
+    public final static String SERVER = "10.120.19.167";
+    public final static String
+            FILE_TO_RECEIVED = "/home/joydebbarman/baghi.mkv";
+
+    public static void main (String [] args ) throws IOException {
+        int bytesRead;
+        int current = 0;
+        FileOutputStream fos = null;
+        BufferedOutputStream bos = null;
+        Socket sock = null;
+        try {
+            sock = new Socket(SERVER, SOCKET_PORT);
+            System.out.println("Connecting...");
+
+            byte [] mybytearray  = new byte [1281199900];
+            InputStream is = sock.getInputStream();
+            fos = new FileOutputStream(FILE_TO_RECEIVED);
+            bos = new BufferedOutputStream(fos);
+            bytesRead = is.read(mybytearray,0,mybytearray.length);
+            current = bytesRead;
+            do {
+                bytesRead =
+                        is.read(mybytearray, current, (mybytearray.length-current));
+                if(bytesRead >= 0) current += bytesRead;
+            } while(bytesRead > -1);
+
+            bos.write(mybytearray, 0 , current);
+            bos.flush();
+            System.out.println("File " + FILE_TO_RECEIVED
+                    + " downloaded (" + current + " bytes read)");
+        }
+        finally {
+            if (fos != null)
+                fos.close();
+            if (bos != null)
+                bos.close();
+            if (sock != null)
+                sock.close();
+        }
+    }
+
+}
+
---
+++ IdeaProject/gs-rest-service/transfer/src/main/java/SendFile.java
@@ -0,0 +1,51 @@
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by joydebbarman on 5/8/16.
+ */
+public class SendFile {
+
+    public final static int SOCKET_PORT = 13267;
+    public final static String FILE_TO_SEND = "/home/joydebbarman/Public/baagi.mkv";
+
+    public static void main (String [] args ) throws IOException {
+
+        try {
+            ServerSocket servsock = new ServerSocket(SOCKET_PORT);
+            while (true) {
+                System.out.println("Waiting for connection...");
+
+                    Socket sock = servsock.accept();
+                    System.out.println("connection is accepted : " + sock);
+                    // send file
+                    File myFile = new File (FILE_TO_SEND);
+                    byte [] mybytearray  = new byte [(int)myFile.length()];
+                    FileInputStream fis = new FileInputStream(myFile);
+                    BufferedInputStream bis = new BufferedInputStream(fis);
+                    bis.read(mybytearray,0,mybytearray.length);
+                    OutputStream os = sock.getOutputStream();
+                    System.out.println("Sending " + FILE_TO_SEND + "(" + mybytearray.length + " bytes)");
+                    os.write(mybytearray,0,mybytearray.length);
+                    os.flush();
+                    System.out.println("Done.");
+
+                if (bis != null) bis.close();
+                if (os != null) os.close();
+                if (sock!=null) sock.close();
+
+                if (servsock != null)
+                    servsock.close();
+            }
+        }
+        catch (Exception e){
+            e.getMessage();
+        }
+
+    }
+}
